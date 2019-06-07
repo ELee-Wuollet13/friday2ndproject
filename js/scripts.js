c1f0dde2b009ea70e9a2db82098c076dcfcacaf5
Student <example@example.com>
2019-06-07 14:19:32 -0700

    hide results for reveal and change text style

--- js/scripts.js
+++ js/scripts.js
@@ -8,32 +8,33 @@ $(document).ready(function() {
     result += parseInt($("input:radio[name=question2]:checked").val());
     console.log(result)
     result += parseInt($("input:radio[name=question3]:checked").val());
-console.log(result)
-    if ($("input:radio[name=gender]:checked").val() === "male") {
+    result += parseInt($("input:radio[name=question4]:checked").val());
+    console.log(result)
+    if ($("input:radio[name=end]:checked").val() === "frontend") {
       if (result >= 2){
-        $(".george").show();
+        $(".Csharp").show();
       }
       else if (result < 2 && result > -2) {
-        $(".jerry").show();
+        $(".Java").show();
       }
       else {
-        $(".peter").show();
+        $(".Python").show();
       }
     }
-     else if ($("input:radio[name=gender]:checked").val() === "female") {
-       if (result >= 2){
-         $(".charlize").show();
-       }
-       else if (result < 2 && result > -2) {
-         $(".jerry").show();
-       }
-       else {
-         $(".cruella").show();
-       }
+    else if ($("input:radio[name=end]:checked").val() === "backend") {
+      if (result >= 2){
+        $(".Go").show();
+      }
+      else if (result < 2 && result > -2) {
+        $(".Java").show();
+      }
+      else {
+        $(".").show();
+      }
     }
     else {
-      $(".peter").show();
+      $(".Ruby").show();
     }
   });
-// var score = parseInt(value)
+  // var score = parseInt(value)
 });
